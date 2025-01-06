a5a101ca515c05fc72aa294ffeded6fe6be3dcc0
suchindra <rsuchindran32@gmail.com>
2025-01-06 15:39:42 +0800

    :sparkles: implemented webpack config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,34 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: false,
   images: {
     domains: ["picsum.photos"],
+  },
+  webpack(config, { isServer }) {
+    // Add support for .ts and .tsx extensions
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    // Add path alias for '@components' to point to 'app/_components'
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@components": path.resolve(__dirname, "app/_components"),
+      "@context": path.resolve(__dirname, "app/_context"),
+    };
+
+    // Add a rule for handling .scss files with style-loader, css-loader, and sass-loader
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+    });
+
+    // Add a rule for handling .svg files using file-loader
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["file-loader"],
+    });
+
+    return config;
   },
 };
 
