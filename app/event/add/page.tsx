7219c4f85ee38ce2e4402bc4340f0c00474a5291
suchindra <rsuchindran32@gmail.com>
2025-02-03 13:35:45 +0800

    :lipstick: implemented draft add event page

--- app/event/add/page.tsx
+++ app/event/add/page.tsx
@@ -1,7 +1,47 @@
+"use client";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import { FC } from "react";
 
 const EventAdd: FC = () => {
-  return <div>Event Add</div>;
+  return (
+    <div>
+      <div>Title</div>
+      <div>
+        <Formik
+          initialValues={{ email: "", password: "" }}
+          validate={(values) => {
+            const errors = { email: null };
+            if (!values.email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form>
+              <Field type="email" name="email" />
+              <ErrorMessage name="email" component="div" />
+              <Field type="password" name="password" />
+              <ErrorMessage name="password" component="div" />
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
+  );
 };
 
 export default EventAdd;
